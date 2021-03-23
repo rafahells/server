00ff74effe9be723bbe8dc0e3de057a56416ea63
Rafael Akira Hanai <rafael@MacBook-Pro-de-Rafael.local>
2021-03-23 12:17:29 +0900

    Deploy

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ app.use('/api/production', production);
 app.use('/api/sales', sales);
 app.use('/api/users', users);
 
+//Handle production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/public/'));
+
+    //Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
